157654dee245b89dcb597a9fe2533ae2ecdb7c60
Sadaqat <sadkb.559@gmail.com>
2025-06-26 18:05:29 +0500

    Update main.py with add student function

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ def show_menu():
     print("2. Delete student")
     print("5. Save & Exit")
 
-Students = []
+students = []
 def main():
     while True:
         show_menu()
         choice = input("Enter your choice: ")
         
         if choice == '1':
-            pass
+            add_student()
         elif choice == '2':
             pass
         elif choice == '3':
@@ -26,4 +26,17 @@ def main():
             print("Invalid choice, please try again.")
             
 main()
+
+def add_student():
+    name = input("Enter student name: ")
+    roll = input("Enter the roll number: ")
+    class_name = input("Enter the class name: ")
+    
+    student = {
+        "name": name,
+        "roll": roll,
+        "class": class_name
+    }
+    students.append(student)
+    print(f"Student {name} added successfully.")
     
