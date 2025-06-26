ba10426ff20e6e1990266d1a0143952261aefb88
Sadaqat <sadkb.559@gmail.com>
2025-06-26 17:36:35 +0500

    First step done: Add basic student record functions

---
+++ main.py
@@ -0,0 +1,29 @@
+def show_menu():
+    print("n====== student management system ======")
+    print("1. Add Student")
+    print("2. View All Students")
+    print("3. Search Student")
+    print("2. Delete student")
+    print("5. Save & Exit")
+
+Students = []
+def main():
+    while True:
+        show_menu()
+        choice = input("Enter your choice: ")
+        
+        if choice == '1':
+            pass
+        elif choice == '2':
+            pass
+        elif choice == '3':
+            pass
+        elif choice == '4':
+            pass
+        elif choice == '5':
+            break
+        else:
+            print("Invalid choice, please try again.")
+            
+main()
+    
